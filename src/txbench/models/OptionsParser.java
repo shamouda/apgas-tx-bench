2f3174a31210a615782c57ad78c9212fcaec325d
u5482878 <sara.salem@anu.edu.au>
2017-06-02 16:38:53 +1000

    fix options parsing

--- src/txbench/models/OptionsParser.java
+++ src/txbench/models/OptionsParser.java
@@ -17,7 +17,7 @@ public class OptionsParser {
         String lastValue = null;
         for (int i = 0; i < args.length; i++) {
             if (args[i].startsWith("-")) {
-                lastKey = args[i]; 
+                lastKey = args[i].replace("-", ""); 
             }
             else if (lastKey != null) {
                 lastValue = args[i];
